2e111bb39ce860e73e8a98573655995e5ddc4b92
GusCardosoSR <gusta_cardoso@hotmail.com>
2025-08-21 16:33:28 -0300

    http para https
    atualização da URL para API

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2E67FHHX.mjs";import"./chunk-LGZH2W2I.mjs";import"./chunk-G5D7ZE25.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-SKPWH66O.mjs";import"./chunk-LGZH2W2I.mjs";import"./chunk-G5D7ZE25.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
