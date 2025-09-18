e743795d3aae3711360881655d1caca8c295c131
João Arthur M Eurich <61804286+TuioDev@users.noreply.github.com>
2025-09-18 05:50:27 -0300

    Dist file
    -Platform frontend file to pull.

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Z5CT5J4A.mjs";import"./chunk-LGZH2W2I.mjs";import"./chunk-G5D7ZE25.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-53ENMNSY.mjs";import"./chunk-2T3H7XTN.mjs";import"./chunk-27YAGH2N.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
