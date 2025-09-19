5c3091e77c0151fb41a113fc79d6e2d6915807be
João Arthur M Eurich <61804286+TuioDev@users.noreply.github.com>
2025-09-18 22:56:53 -0300

    Language support for the data form
    -Data form now will have support for translation, and the language loaded will try to find another language if not found the exact culture one.

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-53ENMNSY.mjs";import"./chunk-2T3H7XTN.mjs";import"./chunk-27YAGH2N.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-466YF4RW.mjs";import"./chunk-2T3H7XTN.mjs";import"./chunk-27YAGH2N.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
