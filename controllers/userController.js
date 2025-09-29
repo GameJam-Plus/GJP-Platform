91b9d1dadce4ab3530883c7e76fc70126736bc64
João Arthur M Eurich <61804286+TuioDev@users.noreply.github.com>
2025-09-29 12:39:16 -0300

    Dist test
    -New dist generated from rodolpho's side.

--- controllers/userController.js
+++ controllers/userController.js
@@ -268,7 +268,7 @@ const getCurrentUser = async (req, res) => {
         if (!user) {
             return res.status(404).json({ success: false, error: 'User Not Found' });
         }
-
+        
         return res.status(200).json({ success: true, data: user });
     } catch (error) {
         return res.status(400).json({ success: false, error: 'Error processing token' });
