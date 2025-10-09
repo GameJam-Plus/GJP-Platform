78b421d6dda22c54c470ba670ea0c548e3f3f79f
João Arthur M Eurich <61804286+TuioDev@users.noreply.github.com>
2025-10-09 16:07:56 -0300

    Preparing new update
    -Remvoing .dist to receive a pull request.

--- dist/gj-platform/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-3PHKMJ2V.mjs";import"./chunk-BKD57EDC.mjs";import"./chunk-Y3WPSK47.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
