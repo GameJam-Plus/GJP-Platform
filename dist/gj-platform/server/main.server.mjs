ee3347ce180618e3f3673cfefc632474a242af00
João Arthur M Eurich <61804286+TuioDev@users.noreply.github.com>
2025-10-17 15:25:26 -0300

    Dist folder
    -Dist folder with updates for the platform.

---
+++ dist/gj-platform/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-Q36VDLUQ.mjs";import"./chunk-BKD57EDC.mjs";import"./chunk-Y3WPSK47.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
