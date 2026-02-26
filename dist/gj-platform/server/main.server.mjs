d9a321b9502e48772413d204ff4aa9b421666c1e
João Arthur M Eurich <61804286+TuioDev@users.noreply.github.com>
2026-02-26 15:01:50 -0300

    Dist folder with updates
    -Updating submission export for the local organizer.

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7PSWAZGE.mjs";import"./chunk-VTL46F7X.mjs";import"./chunk-BRQVQGLR.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-LBA7TTBQ.mjs";import"./chunk-VTL46F7X.mjs";import"./chunk-BRQVQGLR.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
