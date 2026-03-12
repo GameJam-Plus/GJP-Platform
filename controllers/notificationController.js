9a896407789bfd2dc5e60b0a4e88a1f0be8ef7b5
VolpatoSamuel <samuelvol@outlook.com>
2026-03-12 15:28:32 -0300

    Criação dos botões de notificação
    Salvar as notificações na tabela "notificações" para uso indeterminado.

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -2,6 +2,10 @@ const jwt = require('jsonwebtoken');
 const Notification = require('../models/notificationModel');
 const User = require('../models/userModel');
 const test = 0;
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 const createNotification = async (req, res) => {
   const {
     titlePT,
---
+++ models/notificationModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const test = 0;
+const notificationSchema = new Schema({
+  titlePT: {
+    type: String,
+    default: ''
+  },
+  titleES: {
+    type: String,
+    default: ''
+  },
+  titleEN: {
+    type: String,
+    default: ''
+  },
+  descriptionPT: {
+    type: String,
+    default: ''
+  },
+  descriptionES: {
+    type: String,
+    default: ''
+  },
+  descriptionEN: {
+    type: String,
+    default: ''
+  },
+  createdBy: {
+    _id: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+  readBy: [{
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    readAt: {
+      type: Date,
+      default: Date.now
+    }
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Notification', notificationSchema);
